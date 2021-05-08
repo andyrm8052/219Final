cac36fc67ad49f089e15d36dd8bd63823a8c6c1d
andyrm8052 <andy1994>
2021-05-07 23:15:24 -0400

    Commit: Added Session Configuration Added configuration for Session and some external modules.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,20 @@
+/**
+ * Required External Modules
+ */
+
 const express = require('express');
+const path = require("path");
 const open = require('open');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
+
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
 app.use(cors());
 app.use(express.static('docs'));
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -21,3 +32,38 @@ app.listen(app.get('port'), () => {
   console.log('%s: Node server started on %s ...', Date(Date.now()), app.get('port'));
   open('http://localhost:8000');
 });
+
+
+/**
+ * App Variables
+ */
+const port = process.env.PORT || "8000";
+
+
+
+/**
+ * Session Configuration
+ */
+const session = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+}
+
+
+/**
+ * Passport Configuration
+ */
+
+
+
+
+/**
+ *  App Configuration
+ */
