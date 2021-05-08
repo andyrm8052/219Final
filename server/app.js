c7597296243af929c20b8fe976a0c374acf465f3
andyrm8052 <andy1994>
2021-05-07 23:21:15 -0400

    Commit: Added More Configuration for App

--- server/app.js
+++ server/app.js
@@ -90,3 +90,15 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
